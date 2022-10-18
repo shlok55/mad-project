bc0fd9589284557723af1a7f39c9ca98c115569a
aesha1908 <aeshagandhi2003@gmail.com>
2022-10-18 23:16:44 +0530

    Push Code Hello

--- app/src/main/java/com/example/greenrise_sgp/PlantAdapter.java
+++ app/src/main/java/com/example/greenrise_sgp/PlantAdapter.java
@@ -1,5 +1,7 @@
 package com.example.greenrise_sgp;
 
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,25 +11,63 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.DialogPlusBuilder;
+import com.orhanobut.dialogplus.ViewHolder;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.orhanobut.dialogplus.DialogPlus;
 import com.orhanobut.dialogplus.ViewHolder;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Queue;
 
 public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.myViewHolder> {
     ArrayList<Plant> list;
@@ -40,10 +80,10 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.myViewHolder
 
     }
 
-//    public PlantAdapter(DatabaseReference databaseReference) {
-//        this.databaseReference = databaseReference;
-//        databaseReference = FirebaseDatabase.getInstance().getReference();
-//    }
+    public PlantAdapter(DatabaseReference databaseReference) {
+        this.databaseReference = databaseReference;
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+    }
 
     @NonNull
     @Override
@@ -54,7 +94,9 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.myViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull PlantAdapter.myViewHolder holder, int position) {
-       // String key = databaseReference.child("Plants").push().getKey();
+        // String key = databaseReference.child("Plants").push().getKey();
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
 
         Plant plant = list.get(position);
         holder.namev.setText(plant.getName());
@@ -62,9 +104,7 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.myViewHolder
         holder.pricev.setText(String.valueOf(plant.getPrice()));
         holder.quantv.setText(String.valueOf(plant.getQuantity()));
         Glide.with(context).load(plant.getImage()).into(holder.imagev);
-        //String pos = plant.getKey();
-        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference reference = firebaseDatabase.getReference("Plants");
+        //Integer pos = FirebaseDatabase.getInstance().getReference().getRef().getKey();
         holder.updatebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -79,7 +119,6 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.myViewHolder
                 EditText etabout = v.findViewById(R.id.upabout);
                 EditText etprice = v.findViewById(R.id.upprice);
                 EditText etquantity = v.findViewById(R.id.upquantity);
-
                 Button up = v.findViewById(R.id.update);
 
                 etname.setText(plant.getName());
@@ -87,44 +126,45 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.myViewHolder
                 etprice.setText(String.valueOf(plant.getPrice()));
                 etquantity.setText(String.valueOf(plant.getQuantity()));
                 dialogPlus.show();
+                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                DatabaseReference reference = firebaseDatabase.getReference("Plants");
                 up.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-//                        Query query = FirebaseDatabase.getInstance().getReference().child("Plants")
-//                                .orderByChild("key").equalTo(plant.getKey());
-
+                        //Query query = databaseReference.orderByChild("key").equalTo(plant.getKey());
                         reference.addListenerForSingleValueEvent(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                for(DataSnapshot dataSnapshot : snapshot.getChildren())
+                                for(DataSnapshot dataSnapshot: snapshot.getChildren())
                                 {
-                                    if (dataSnapshot.child("name").getValue().toString().equals(holder.namev.getText()))
+                                    if(dataSnapshot.child("key").getValue().toString().equals(plant.getKey()))
                                     {
-                                        HashMap map = new HashMap();
+                                       // String path = dataSnapshot.getKey();
+                                       // DatabaseReference reference = dataSnapshot.getRef();
+                                        Map<String,Object> map = new HashMap<>();
                                         map.put("name",etname.getText().toString());
                                         map.put("about",etabout.getText().toString());
                                         map.put("price",etprice.getText().toString());
                                         map.put("quantity",etquantity.getText().toString());
-                                        reference.child(dataSnapshot.child("key").getValue().toString()).updateChildren(map)
-                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                    @Override
-                                                    public void onComplete(@NonNull Task<Void> task) {
-                                                        Toast.makeText(holder.namev.getContext(),"Done",Toast.LENGTH_SHORT);
-                                                        dialogPlus.dismiss();
-                                                    }
-                                                })
-                                                .addOnFailureListener(new OnFailureListener() {
-                                                    @Override
-                                                    public void onFailure(@NonNull Exception e) {
-                                                        Toast.makeText(holder.namev.getContext(),"Oops!",Toast.LENGTH_SHORT);
-                                                        dialogPlus.dismiss();
-                                                    }
-                                                });
+                                        reference.child(dataSnapshot.child("key").getValue().toString())
+                                                .updateChildren(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                try{
+                                                    Toast.makeText(holder.namev.getContext(), "Done", Toast.LENGTH_SHORT).show();
+                                                    dialogPlus.dismiss();
+                                                }
+                                                catch(DatabaseException e){
+                                                    //Log the exception and the key
+                                                    dataSnapshot.getKey();
+                                                }
+
+                                            }
+                                        });
                                     }
 
+
                                 }
-
-
                             }
 
                             @Override
@@ -133,27 +173,26 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.myViewHolder
                             }
                         });
 
-
                     }
                 });
-              //  dialogPlus.show();
+
             }
         });
         holder.delbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 databaseReference.child("Plants").child(plant.getKey()).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        Toast.makeText(holder.namev.getContext(), "Data Deleted", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(holder.namev.getContext(), "Error occured!"+e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(holder.namev.getContext(), "Data Deleted", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(holder.namev.getContext(), "Error occured!"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
     }
